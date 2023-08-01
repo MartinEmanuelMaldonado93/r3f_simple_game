427933bd09cecba56f6d876e91be706559546f6f
Martin Maldonado <martinemanuel93@gmail.com>
2023-08-01 18:37:39 -0300

    fix types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+"use client";
+import { Physics, useBox, usePlane, useSphere } from "@react-three/cannon";
+import Debug from "@react-three/cannon/dist/debug-provider";
+import { OrbitControls } from "@react-three/drei";
+import {
+	BoxBufferGeometryProps,
+	BoxGeometryProps,
+	Canvas,
+	MeshProps,
+	MeshStandardMaterialProps,
+	SphereBufferGeometryProps,
+	SphereGeometryProps,
+	useFrame,
+	useThree,
+} from "@react-three/fiber";
+import { useEffect } from "react";
+import { BoxGeometry, Mesh, MeshStandardMaterial, SphereGeometry } from "three";
+// import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<>
+			<Canvas
+				style={{ height: "100vh" }}
+				camera={{ position: [0, 5, 12], fov: 60 }}
+			>
+				<Lights />
+				{/* <gridHelper /> */}
+				<Physics
+					gravity={[0, -30, 0]}
+					defaultContactMaterial={{ restitution: 1.1 }}
+				>
+					<Ball />
+					<Enemy color={"hotpink"}  />
+					<Paddle />
+				</Physics>
+				<OrbitControls enableRotate={false} />
+			</Canvas>
+		</>
+	);
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Ball({
+	args = [0.5, 32, 32],
+	...props
+}: SphereBufferGeometryProps & Omit<MeshProps, "args">) {
+	const [ref, api] = useSphere<Mesh>(() => ({ args: [0.5], mass: 1 }));
+	const { viewport } = useThree();
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	// Physic plane to detect when fall down
+	usePlane(() => ({
+		position: [0, -viewport.height, 0],
+		rotation: [-Math.PI / 2, 0, 0],
+		onCollide: () => {
+			api.position.set(0, 0, 0);
+			api.velocity.set(0, 10, 0);
+		},
+	}));
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<mesh ref={ref} {...props}>
+			<sphereGeometry args={args} />
+			<meshStandardMaterial color="orange" />
+		</mesh>
+	);
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+function Paddle({
+	args = [2, 0.5, 1],
+	...props
+}: MeshProps & BoxBufferGeometryProps) {
+	const [ref, api] = useBox<Mesh>(() => ({ args }));
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	useFrame((state) => {
+		api.position.set(
+			(state.mouse.x * state.viewport.width) / 2.0,
+			-state.viewport.height / 3,
+			0
+		);
+		api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 5);
+	});
+	return (
+		<mesh ref={ref} {...props}>
+			<boxBufferGeometry args={args} />
+			<meshStandardMaterial color="lighblue" />
+		</mesh>
+	);
+}
+
+function Enemy({
+	color,
+	position,
+	...props
+}: BoxBufferGeometryProps & Omit<MeshProps, "args">) {
+	const [ref, api] = useBox<Mesh>(() => ({ position: [2, 1, 0] }));
+
+	return (
+		<mesh ref={ref} {...props} position={[2, 1, 0]}>
+			<boxGeometry args={[2, 0.5, 1]} />
+			<meshStandardMaterial color={color} />
+		</mesh>
+	);
+}
+
+function Lights() {
+	return (
+		<>
+			<color attach={"background"} args={["black"]} />
+			<ambientLight intensity={0.3} />
+			<pointLight position={[10, 10, 5]} />
+			<pointLight position={[-10, -10, -5]} color={"red"} />
+		</>
+	);
 }
